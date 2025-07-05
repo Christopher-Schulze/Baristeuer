e581c78e2a1412b1d1c08bc01abdd4723c878cb2
Christopher <schulze.christopher@icloud.com>
2025-07-06 00:07:08 +0200

    Add settings e2e test

--- internal/ui/playwright.config.js
+++ internal/ui/playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
+  testMatch: ["**/*.spec.js"],
   timeout: 30000,
   use: {
     baseURL: "http://localhost:5173",
