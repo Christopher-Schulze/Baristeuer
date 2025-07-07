c4333ea86778c56ef2c9bad9278d07efeaf4aa9a
Christopher <schulze.christopher@icloud.com>
2025-07-07 23:26:42 +0200

    Format csv upload plugin with gofmt

--- internal/plugins/csvupload/plugin/main.go
+++ internal/plugins/csvupload/plugin/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-    "baristeuer/internal/plugins"
-    "baristeuer/internal/plugins/csvupload"
+	"baristeuer/internal/plugins"
+	"baristeuer/internal/plugins/csvupload"
 )
 
 // New is the symbol loaded by the application.
 func New() plugins.Plugin {
-    return csvupload.New()
+	return csvupload.New()
 }
-
